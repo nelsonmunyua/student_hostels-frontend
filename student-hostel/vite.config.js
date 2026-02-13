6834d2ec9e87a911be7adba659f15018251d30cc
nelson <nelsonmunyua8@gmail.com>
2026-02-13 12:40:51 +0300

    fixed deployment

--- student-hostel/vite.config.js
+++ student-hostel/vite.config.js
@@ -7,11 +7,14 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
           redux: ['@reduxjs/toolkit', 'react-redux'],
+          icons: ['lucide-react'],
         },
       },
     },
