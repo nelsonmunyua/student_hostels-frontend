c03d5e7920ff57af53223f3a09aa9f648c0c4dbf
nelson <nelsonmunyua8@gmail.com>
2026-02-13 10:49:10 +0300

    Fixed deployment

--- student-hostel/vite.config.js
+++ student-hostel/vite.config.js
@@ -4,5 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: process.env.VITE_BASE_PATH || "/student-hostel",
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    host: true,
+  },
 })
